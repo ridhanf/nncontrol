9817fce3c6769eeb6a07378710d248ed28c77732
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2020-04-23 19:31:36 +0700

    Use NeuPy and create Optimizer

--- Source/optimizer.py
+++ Source/optimizer.py
@@ -1,2 +1,12 @@
-from scipy.optimize import curve_fit
+import numpy as np
+from neupy import algorithms
+from neupy.layers import *
 
+x_train = np.array([[1, 2], [3, 4]])
+y_train = np.array([[1], [0]])
+
+optimizer = algorithms.QuasiNewton(
+    Input(2) >> Sigmoid(3) >> Sigmoid(1),
+    update_function='bfgs')
+
+optimizer.train(x_train, y_train, epochs=10)
