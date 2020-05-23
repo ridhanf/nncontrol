1e5d1b25c93cb8754d1b31eac122f8832d086d6b
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2020-05-23 14:05:00 +0700

    Feedback Linearization 50%

--- Source/model_training.py
+++ Source/model_training.py
@@ -1,5 +1,5 @@
 # Import needed libraries
-from fireTS.models import NARX
+from fireTS.models import DirectAutoRegressor
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -12,8 +12,8 @@ data = pd.read_excel('../Data/data.xlsx')
 # y = data.loc[:, ['Td','RH'] ]
 # X = data.loc[:, ['HT','AC','To','RD'] ]
 
-y = data.loc[:, ['Td'] ]
-X = data.loc[:, ['AC'] ]
+y = data.loc[:, ['Td','RH'] ]
+X = data.loc[:, ['HT','AC'] ]
 
 # Split dataset
 # Data test
@@ -23,7 +23,8 @@ X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=(15/
 
 # print proportions
 print()
-print('train: {}% | validation: {}% | test {}%'.format(round(len(y_train)/len(y)*100,2),
+print('Dataset splitting configuration')
+print('Train: {}% | Validation: {}% | Test {}%'.format(round(len(y_train)/len(y)*100,2),
                                                         round(len(y_val)/len(y)*100,2),
                                                         round(len(y_test)/len(y)*100,2)))
 print()
@@ -44,42 +45,56 @@ def kinerja(y_target, y_pred, method="evar"):
 ask_train = input("Train the model? (y/n):")
 if ask_train == 'y':
     # Model design (MLP-NARX)
-    mdl_MLP = MLPRegressor(random_state = 1, hidden_layer_sizes=(91), max_iter = 5000)
+    MLP_Td = MLPRegressor(random_state = 1, hidden_layer_sizes=(91), max_iter = 5000)
+    MLP_RH = MLPRegressor(random_state = 1, hidden_layer_sizes=(91), max_iter = 5000)
 
-    p_value, q_value, d_value = 1, [1]*4, [0]*4
-    p_value, q_value, d_value = 1, [1], [0]
-    mdl_NARX = NARX(mdl_MLP, auto_order=p_value, exog_order=q_value, exog_delay=d_value)
+    p_value, q_value, d_value = 1, [1]*2, [0]*2
+    NARX_Td = DirectAutoRegressor(MLP_Td, auto_order=p_value, exog_order=q_value, exog_delay=d_value, pred_step=1)
+    NARX_RH = DirectAutoRegressor(MLP_RH, auto_order=p_value, exog_order=q_value, exog_delay=d_value, pred_step=1)
 
     # Model Training
-    print('Training Model JST...')
-    # mdl_MLP.fit(X_temp, y_temp)
-    mdl_NARX.fit(X_train, y_train.Td)
-
+    print('Model (Td) training...')
+    NARX_Td.fit(X_train, y_train.Td)
+    print('Model (RH) training...')
+    NARX_RH.fit(X_train, y_train.RH)
 
     # Prediction
-    # y_pred = mdl_MLP.predict(X_val)
-    y_pred = mdl_NARX.predict(X_val, y_val.Td, step=1)
+    Td_pred = NARX_Td.predict(X_val, y_val.Td)
+    RH_pred = NARX_RH.predict(X_val, y_val.RH)
+    print()
 
-
-    # Performance Evaluation
-    EVar = round(kinerja(y_val.Td, y_pred, method='evar')*100, 2)
-    R2   = round(kinerja(y_val.Td, y_pred, method='r2')*100, 2)
-    RMSE = round(kinerja(y_val.Td, y_pred, method='mse')**0.5, 2)
-    MAE  = round(kinerja(y_val.Td, y_pred, method='mae'), 2)
-    print("Performance evaluation based on Validation Data")
+    # Td Performance Evaluation
+    EVar = round(kinerja(y_val.Td, Td_pred, method='evar')*100, 2)
+    R2   = round(kinerja(y_val.Td, Td_pred, method='r2')*100, 2)
+    RMSE = round(kinerja(y_val.Td, Td_pred, method='mse')**0.5, 2)
+    MAE  = round(kinerja(y_val.Td, Td_pred, method='mae'), 2)
+    print("Td Performance evaluation based on Validation Data")
     print("EVar = {}% | R2 = {}% | RMSE = {} | MAE = {}".format(EVar, R2, RMSE, MAE))
     print()
 
-    ask_simpan = input("Simpan model? (y/n):")
+    # RH Performance Evaluation
+    EVar = round(kinerja(y_val.RH, RH_pred, method='evar')*100, 2)
+    R2   = round(kinerja(y_val.RH, RH_pred, method='r2')*100, 2)
+    RMSE = round(kinerja(y_val.RH, RH_pred, method='mse')**0.5, 2)
+    MAE  = round(kinerja(y_val.RH, RH_pred, method='mae'), 2)
+    print("RH Performance evaluation based on Validation Data")
+    print("EVar = {}% | R2 = {}% | RMSE = {} | MAE = {}".format(EVar, R2, RMSE, MAE))
+    print()
+
+    ask_simpan = input("Save the model? (y/n):")
     if ask_simpan == 'y':
-        print('Menyimpan model JST...')
+        print('Saving NARX Model...')
         # Menyimpan model yang sudah dibuat
         import pickle
 
-        pkl_filename = "model.pkl"
-        with open(pkl_filename, 'wb') as file:
-            pickle.dump(mdl_NARX, file)
-            # pickle.dump(mdl_MLP, file)
-            print('Model JST tersimpan!')
+        with open("model_Td.pkl", 'wb') as file:
+            pickle.dump(NARX_Td, file)
+            print('NARX model (Td) saved!')
+
+        with open("model_RH.pkl", 'wb') as file:
+            pickle.dump(NARX_RH, file)
+            print('NARX model (RH) saved!')
     else:
-        print('Model JST tidak disimpan.')
+        print('Model not saved.')
+else:
+    print('Model not trained.')
